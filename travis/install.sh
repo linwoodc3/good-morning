57abfd178e809ea6b19756fef05c6914847a6d5e
Linwood Creekmore III <valinvescap@gmail.com>
2017-07-23 12:12:17 -0400

    Fixing the travis ci build. # 11

--- travis/install.sh
+++ travis/install.sh
@@ -4,26 +4,23 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     # Install some custom requirements on OS X
     # e.g. brew install pyenv-virtualenv
-
+    brew install python3
+    echo alias python='python3' >>~/.bash_profile
+    echo alias pip='pip3' >>~/.bash_profile
+    source ~/.bash_profile
 
     case "${GDELT}" in
         py34)
             # Install some custom Python 3.4 requirements on OS X
             pip install pip -U
-            brew install python3
-            alias python='python3'
             ;;
         py35)
             # Install some custom Python 3.5 requirements on OS X
             pip install pip -U
-            brew install python3
-            alias python='python3'
             ;;
         py36)
             # Install some custom Python 3.6 requirements on OS X
             pip install pip -U
-            brew install python3
-            alias python='python3'
             ;;
     esac
 else
@@ -31,8 +28,13 @@ else
 
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get update -q -y
+    sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable -y
     sudo apt-get install python-dev -y
+    sudo apt-get install gcc-4.8 -y
     sudo apt-get clean	-y
-pip install numpy -U
-pip install -r requirements.txt
+fi
+
+if [ "$GDELT" == py27 ]; then
+        echo 'geo testing'; pip install -r requirements_geo.txt;
+        else pip install -r requirements.txt;
 fi;
